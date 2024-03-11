45b53a901b1ecf44aa7b45119a500a2fb1e319dc
Amarjeet Rao <amarjeetrao@outlook.com>
2024-03-12 02:22:23 +0530

    java basic exercise second code

--- Java-Basic-Exercises/src/Main.java
+++ Java-Basic-Exercises/src/Main.java
@@ -9,6 +9,21 @@ public class Main {
         SpeedConverter speed = new SpeedConverter();
         speed.printConversion(10);
 
+        MegaBytesConverter megabyte= new MegaBytesConverter();
+        megabyte.printMegaBytesAndKiloBytes(100);
+
+        LeapYear leapYear = new LeapYear();
+        leapYear.isLeapYear(2024);
+
+        BarkingDog barkingDog = new BarkingDog();
+        barkingDog.shouldWakeUp(true, 5);
+
+        DecimalComparator decimalComparator = new DecimalComparator();
+        decimalComparator.areEqualByThreeDecimalPlaces(2203.33,4455.566);
+
+        EqualSumChecker equalSumChecker = new EqualSumChecker();
+        equalSumChecker.hasEqualSum(222,444,6644);
+
 
     }
 }
